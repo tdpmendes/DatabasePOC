117ea99ac792c952e5cf7c22ac62585058ffe43e
Tácio Dias Palhão Mendes <taciomendes@hotmail.com>
2023-01-25 19:50:32 +0000

    formatação

--- EFPocContext.cs
+++ EFPocContext.cs
@@ -5,7 +5,7 @@ namespace EFPoc
     public class EFPocContext : DbContext
     {
         public virtual DbSet<Aluno> Alunos { get; set; }
-        public virtual DbSet<Disciplina> Disciplinas{ get; set; }
+        public virtual DbSet<Disciplina> Disciplinas { get; set; }
         public virtual DbSet<Horario> Horarios { get; set; }
         public virtual DbSet<Professor> Professores { get; set; }
         public virtual DbSet<Turma> Turmas { get; set; }
